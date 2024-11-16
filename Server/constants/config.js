b2331e902896dd8f4f0367f747ee77967df9714a
Arijit Bouri <137565275+arijitbouri0@users.noreply.github.com>
2024-11-16 21:07:48 +0530

    Update config.js

--- Server/constants/config.js
+++ Server/constants/config.js
@@ -1,9 +1,8 @@
 const corsOptions = {
     origin: [
-        'https://mern-chat-app-kd4r.vercel.app'
+        process.env.CLIENT_URL,
     ],
     credentials: true,
-
 }
 
-export {corsOptions}
+export { corsOptions }
