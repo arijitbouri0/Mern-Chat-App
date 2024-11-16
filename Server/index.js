abdcde529c51dec37c64ceeb7ab88bfa6e530069
Arijit Bouri <137565275+arijitbouri0@users.noreply.github.com>
2024-11-16 14:46:37 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -53,6 +53,9 @@ cloudinary.config({
 
 
 //routes
+app.use("/", (req, res) => {
+  return res.send("Server is running!");
+});
 app.use('/api/user', UserRouter);
 app.use('/api/chat', ChatRouter);
 
